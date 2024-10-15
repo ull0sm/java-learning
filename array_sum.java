fd4e149cf02f82fdb7aa325f4ef0430cb7ee189b
ull0sm <ullas4101997@gmail.com>
2024-10-15 15:30:39 +0530

    2nd problem

---
+++ array_sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class array_sum {
+    static Scanner scn = new Scanner(System.in);
+    public static int ans(int n){
+        int sum =0;
+        int[] arr = new int[n];
+        System.out.printf("Enter the %d element:\n",n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(i+1+":");
+            arr[i] = array_sum.scn.nextInt();
+            sum = sum+arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the value of n:");
+        int n = array_sum.scn.nextInt();
+        int[] arr = new int[n];
+        System.out.println("the sum: "+ans(n));
+    }
+}
