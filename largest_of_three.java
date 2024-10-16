07fd778c33de572a86ff112c2bbb4662f7fbc447
ull0sm <ullas4101997@gmail.com>
2024-10-16 12:30:54 +0530

    largest and smallest

--- largest_of_three.java
+++ largest_of_three.java
@@ -4,20 +4,34 @@ public class largest_of_three {
     static Scanner sc = new Scanner(System.in);
 
     public static int largest(int a,int b,int c){
-        if(a>b && b>c){
+        if(a>=b && b>=c){
             return a;
         }
-        else if (b>c && b>a) {
+        else if (b>=c && b>=a) {
+            return b;
+        }
+        return c;
+    }
+    public static int smallest(int a,int b,int c){
+        if(a<=b && b<=c){
+            return a;
+        }
+        else if (b<=c && b<=a) {
             return b;
         }
         return c;
     }
     public static void main(String[] args) {
         System.out.println("Enter the three number:");
-        int x,y,z;
-        x = sc.nextInt();
-        y = sc.nextInt();
-        z = sc.nextInt();
-        System.out.printf("The larges of %d %d %d is %d",x,y,z,largest(x, y, z));
+        int x=0,y=0,z=0;
+        try {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            z = sc.nextInt();  
+        } catch (Exception e) {
+            System.out.println("please enter only integers!!!");
+        }
+        System.out.printf("The largest of %d %d %d is %d",x,y,z,largest(x, y, z));
+        System.out.printf("\nThe smallest of %d %d %d is %d",x,y,z,smallest(x, y, z));
     }
 }
