47abf5be69cd73d08bda41ddd72c2d18f378b2b5
ull0sm <ullas4101997@gmail.com>
2024-10-17 10:53:36 +0530

    updated palindrome

--- palindrome.java
+++ palindrome.java
@@ -2,27 +2,19 @@ import java.util.Scanner;
 
 public class palindrome {
     static Scanner sc = new Scanner(System.in);
-    public static void ispalindrome(String x){
+    public static boolean ispalindrome(String x){
         int ans = 0;
         int n = x.length();
         for (int i = 0; i < n/2; i++) {
-            if(x.charAt(i)==x.charAt(n-i-1)){
-                ans = 1;
-            }
-            else{
-                ans = 0;
+            if(x.charAt(i)!=x.charAt(n-i-1)){
+                return false;
             }
         }
-        if(ans != 0){
-            System.out.println("true");
-        }
-        else{
-            System.out.println("false");
-        }
+        return true;
     }
     public static void main(String[] args) {
         System.out.println("enter the string:");
         String str = sc.next();
-        ispalindrome(str);
+        System.out.println(ispalindrome(str));
     }
 }
