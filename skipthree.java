c3cfb225eb1a7d09410264c96d59743a7e427064
ull0sm <ullas4101997@gmail.com>
2024-10-20 11:40:16 +0530

    skipping three

---
+++ skipthree.java
@@ -0,0 +1,14 @@
+
+public class skipthree {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 21; i++) {
+            if (i % 3 == 0) {   //change sign to !=
+                continue; // remove this
+                // System.out.printf("%d ",i);
+
+            }
+            System.out.printf("%d ", i);
+        }
+    }
+}
